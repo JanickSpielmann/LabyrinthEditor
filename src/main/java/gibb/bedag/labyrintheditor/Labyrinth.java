8e8b673506213a929b679edd1cd126c22f349b5a
Okami <mcendrine27@gmail.com>
2023-09-11 21:42:13 +0200

    feat: Labyrinth Klasse wurde erstellt, mit allen vorgegebenen Attributen

---
+++ src/main/java/gibb/bedag/labyrintheditor/Labyrinth.java
@@ -0,0 +1,37 @@
+package gibb.bedag.labyrintheditor;
+
+public class Labyrinth {
+    private long width;
+    private long height;
+    private String[] tiles;
+
+    public Labyrinth(long width, long height, String[] tiles) {
+        this.width = width;
+        this.height = height;
+        this.tiles = tiles;
+    }
+
+    public long getWidth() {
+        return width;
+    }
+
+    public void setWidth(long width) {
+        this.width = width;
+    }
+
+    public long getHeight() {
+        return height;
+    }
+
+    public void setHeight(long height) {
+        this.height = height;
+    }
+
+    public String[] getTiles() {
+        return tiles;
+    }
+
+    public void setTiles(String[] tiles) {
+        this.tiles = tiles;
+    }
+}
