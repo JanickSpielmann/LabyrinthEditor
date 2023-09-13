46e73210be34562c0e7fdd63d52f2062dc68cc23
Okami <mcendrine27@gmail.com>
2023-09-13 19:53:08 +0200

    feat: Merge Request korrekturen

--- src/main/java/gibb/bedag/labyrintheditor/Tile.java
+++ src/main/java/gibb/bedag/labyrintheditor/Tile.java
@@ -2,9 +2,13 @@ package gibb.bedag.labyrintheditor;
 
 public class Tile {
     private boolean isFree;
+    private long width;
+    private long height;
 
-    public Tile(boolean isFree) {
-        this.isFree = isFree;
+    public Tile() {
+        this.isFree = true;
+        this.width = 20;
+        this.height = 20;
     }
 
     public boolean isFree() {
@@ -13,5 +17,21 @@ public class Tile {
 
     public void setFree(boolean free) {
         isFree = free;
+    }
+
+    public long getWidth() {
+        return width;
+    }
+
+    public void setWidth(long width) {
+        this.width = width;
+    }
+
+    public long getHeight() {
+        return height;
+    }
+
+    public void setHeight(long height) {
+        this.height = height;
     }
 }
