e669df20a8ea3b9d0893630fe69341bbb65f8648
Efecan Erken <eer141900@stud.gibb.ch>
2023-09-15 11:37:09 +0200

    feat: Klassenvariable 'exists' inkl. dazugehörige Methoden entfernt

--- src/main/java/gibb/bedag/labyrintheditor/DestructableBlock.java
+++ src/main/java/gibb/bedag/labyrintheditor/DestructableBlock.java
@@ -2,19 +2,9 @@ package gibb.bedag.labyrintheditor;
 
 public class DestructableBlock extends Block {
 
-    private boolean exists;
 
     public DestructableBlock(int width, int height) {
         super(width, height);
         state = State.DESTRUCTABLE;
-        exists = true;
-    }
-
-    public boolean isExists() {
-        return exists;
-    }
-
-    public void setExists(boolean exists) {
-        this.exists = exists;
     }
 }
