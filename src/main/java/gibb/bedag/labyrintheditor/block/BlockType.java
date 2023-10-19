c125a7cc51bb4a714fb448670a42e695c27ac3be
cendrine.meystre <cme138579@stud.gibb.ch>
2023-10-19 23:00:12 +0200

    feat: improved Blocks and moved toString to enum value

--- src/main/java/gibb/bedag/labyrintheditor/block/BlockType.java
+++ src/main/java/gibb/bedag/labyrintheditor/block/BlockType.java
@@ -1,6 +1,12 @@
 package gibb.bedag.labyrintheditor.block;
 
 public enum BlockType {
-    DESTRUCTABLE,
-    INDESTRUCTABLE
+    DESTRUCTIBLE("1"),
+    INDESTRUCTIBLE("2");
+
+    public final String value;
+
+    BlockType(String value){
+        this.value = value;
+    }
 }
--- src/main/java/gibb/bedag/labyrintheditor/block/IndestructableBlock.java
+++ src/main/java/gibb/bedag/labyrintheditor/block/IndestructableBlock.java
@@ -2,8 +2,7 @@ package gibb.bedag.labyrintheditor.block;
 
 public class IndestructableBlock extends Block {
 
-    public IndestructableBlock(int width, int height) {
-        super(width, height);
-        state = State.INDESTRUCTABLE;
+    public IndestructableBlock() {
+        blockType = BlockType.INDESTRUCTIBLE;
     }
 }
